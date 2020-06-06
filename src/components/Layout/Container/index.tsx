3b8e16fa59d9319294783ba26a222ca00c478ef6
Dmitri Pavlutin <dpavlutin@gmail.com>
2020-06-06 16:49:11 +0300

    Use tailwind to layout

--- src/components/Layout/Container/index.tsx
+++ src/components/Layout/Container/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 import 'styles/index.scss';
-import styles from './index.module.scss';
 
 import Footer from 'components/Layout/Footer';
 import LayoutHeader from 'components/Layout/Header';
@@ -28,10 +27,10 @@ export default function LayoutContainer({
     <>
       <LayoutMetaTags siteInfo={siteInfo} />
       <LayoutHeader authorProfilePicture={authorProfilePicture} siteInfo={siteInfo} />
-      <div className={styles.container}>
-        <main className={styles.main}>{children}</main>
-        <aside className={styles.leftSidebar}>{leftSidebar}</aside>
-        <aside className={styles.rightSidebar}>{rightSidebar}</aside>
+      <div className="flex h-full">
+        <main className="order-2 w-full max-w-3xl flex-shrink-0 mx-auto px-4">{children}</main>
+        <aside className="order-1 w-80 flex-grow flex-shrink hidden lg:block">{leftSidebar}</aside>
+        <aside className="order-2 w-80 flex-grow flex-shrink-0 hidden lg:block">{rightSidebar}</aside>
       </div>
       <Footer authorInfo={authorInfo} />
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '80': '20rem'
+      }
+    },
   },
   variants: {},
   plugins: [],
